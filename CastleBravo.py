f17bacc949c4f1d7fb126f4349d453f46ac11eae
caruso <andrew32408@gmail.com>
2024-10-15 08:08:58 -0400

    Added More Elif and an Else Statement to The VRS Functionn - Stable

--- CastleBravo.py
+++ CastleBravo.py
@@ -20,12 +20,31 @@ def vehicleResponseSystem():
         print("\n The National Weather Service has updated alarm by 30 minutes because"
               " of the Forecast of", weatherAlert, "weather conditions.")
         sleep(1)
-        print("\n VRS System Has Been Engaged Only Allowing You to Drive 55-MPH.")
+        print("\n VRS System Has Been Engaged, Only Allowing You to Drive 55-MPH.")
     elif weatherAlert == "blizzard":
         print("\n The National Weather Service has updated alarm by 45 minutes because"
               " of the Forecast of", weatherAlert, "weather conditions.")
         sleep(1)
-        print("\n VRS System Has Been Engaged Only Allowing You to Drive 45-MPH.")
-
+        print("\n VRS System Has Been Engaged, Only Allowing You to Drive 45-MPH.")
+    elif weatherAlert == "rainy":
+        print("\n The National Weather Service has updated alarm by 15 minutes because"
+              " of the Forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\n VRS System Has Been Engaged, Only Allowing You to Drive 65-MPH.")
+    elif weatherAlert == "windy":
+        print("\n The National Weather Service has updated alarm by 10 minutes because"
+              " of the Forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\n VRS System Has Been Engaged, Only Allowing You to Drive 70-MPH.")
+    elif weatherAlert == "icy":
+        print("\n The National Weather Service has updated alarm by 50 minutes because"
+              " of the Forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\n VRS System Has Been Engaged Only Allowing You to Drive 30-MPH.")
+    else:
+        print("\n The National Weather Service is Calling for", weatherAlert, "Skies, Drive Carefully to get to your Destination!")
+        sleep(1)
+        print("\n VRS System Has Been Disengaged."
+              "")
 
 vehicleResponseSystem()
